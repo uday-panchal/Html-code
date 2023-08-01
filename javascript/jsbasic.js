050fce51675cb03cfcdf62e165fa482538af1ffe
unknown <panchaluday269@example.com>
2023-08-01 10:36:53 +0530

    pracctice code added

--- javascript/jsbasic.js
+++ javascript/jsbasic.js
@@ -2,4 +2,34 @@ let x =10;
 console.log(x);
 
 let y = true;
-console.log(y)
+console.log(y);
+{
+let x =10;
+console.log(x);
+}
+{
+let x =10;
+}
+console.log(x)
+{
+var name ="uday"
+var number =10;
+console.log(number)
+}
+
+let a ="10ab"
+let b =10;
+let c = parseInt(a) + parseFloat(b);
+console.log("c" , c)
+
+var myName ="harsh"
+var myName;
+console.log(myName)
+
+
+let d="50"+3+6;
+console.log(d)
+
+
+var z = 10;
+console.log(z)
